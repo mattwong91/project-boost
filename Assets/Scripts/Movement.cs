2ff7ba2108dc41258585dd3a0d0c8ae4537cef81
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-04-08 00:06:39 -0600

    Accept Input for thrust and rotation

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    ProcessThrust();
+    ProcessRotation();
+  }
+
+  void ProcessThrust()
+  {
+    if (Input.GetKey(KeyCode.Space))
+    {
+      Debug.Log("Pressed Space - Thrust");
+    }
+  }
+
+  void ProcessRotation()
+  {
+    if (Input.GetKey(KeyCode.A))
+    {
+      Debug.Log("Pressed A - Rotate Left");
+    }
+    else if (Input.GetKey(KeyCode.D))
+    {
+      Debug.Log("Pressed D - Rotate Right");
+    }
+  }
+}
