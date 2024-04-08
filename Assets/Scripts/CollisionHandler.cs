d5ccd6da6ec88cf4aa89662ad5a3ee5f84b7dadf
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-04-08 02:29:54 -0600

    Using Switch with tags

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,5 +2,19 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-
+  void OnCollisionEnter(Collision other)
+  {
+    switch (other.gameObject.tag)
+    {
+      case "Friendly":
+        Debug.Log("This is friendly");
+        break;
+      case "Finish":
+        Debug.Log("You reached the landing pad");
+        break;
+      default:
+        Debug.Log("You collided with something");
+        break;
+    }
+  }
 }
